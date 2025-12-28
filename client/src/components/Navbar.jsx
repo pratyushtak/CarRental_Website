f1206ab69538bd71b797244ec54c19c53d638aa5
Pratyush Tak <pratyushtak4@gmail.com>
2025-12-28 22:29:58 +0530

    front-end DAY 2

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { menuLinks } from '../assets/assets'
 import Link from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <Link to='/'>
-            <img src={assets.logo} alt="logo" className='h-8'/>   {/*Why?*/}
+            <img src={assets.logo} alt="logo" className='h-8'/>
         </Link>
-
+        
         <div>
-
+          {menuLinks.map((link, index)=>
+          (
+            <Link key = {index} to={link.path}>
+              {link.name}
+            </Link>
+          ))}
         </div>
     </div>
   )
