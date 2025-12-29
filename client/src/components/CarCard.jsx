a98bec29ecd68437649c758de9a9f8255a34dfa0
Pratyush Tak <pratyushtak4@gmail.com>
2025-12-29 23:23:35 +0530

    front_end

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
 const CarCard = ({car}) => {
+  const currency = import.meta.env.VITE_CURRENCY
   return (
-    <div>CarCard</div>
+    <div className='group rounded-x1 overflow-hidden shadow-lg hover:-translate-y-1
+    transition-all duration-500 cursor-pointer'>
+        <div className='relative h-48 overflow-hidden'>
+          <img src={car.image} alt="Car Image" className='w-full h-full object-cover transition-transform duration-500 
+          group-hover:scale-105'/>
+
+
+          {car.isAvailable && <p className='absolute top-4 left-4 bg-primary/90 text-white 
+          text-xs px-2.5 py-1 rounded-full'>
+            Available Now
+          </p>}
+
+          <div className="absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white
+            px-3 py-2 rounded-lg">
+              <span className='font-semibold'>{currency}{car.pricePerDay}</span>
+              <span className='text-sm text-white/80'> / day</span>
+        </div>
+      </div>
+
+
+      <div className='p-4 sm:p-5'>
+          <div className='flex justify-between items-start mb-2'> 
+              <div className=''>
+                <h3>{car.brand} {car.model}</h3>
+                <p>{car.category} • {car.year}</p>
+              </div>
+          </div>
+
+          <div>
+
+          </div>
+      </div>
+
+    </div>
   )
 }
 
