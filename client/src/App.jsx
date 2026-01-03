5ee708df2922a3d1288329b6a2922c963c7d62c7
Pratyush Tak <pratyushtak4@gmail.com>
2026-01-03 23:40:25 +0530

    frontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import { Footer } from './components/Footer'
 
 const App = () => {
 
@@ -21,6 +22,8 @@ const App = () => {
           <Route path='/cars' element={<Cars />}/>
           <Route path='/my-bookings' element={<MyBookings />}/>
        </Routes>
+
+       {!isOwnerPath && <Footer /> }
     </>
 
   )
