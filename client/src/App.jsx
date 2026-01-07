ad27658939c6ae6920477f1f35b19393a30327e8
Pratyush Tak <pratyushtak4@gmail.com>
2026-01-07 15:56:38 +0530

    project files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Dashboard from './pages/owner/Dashboard'
 import AddCar from './pages/owner/AddCar'
 import ManageCars from './pages/owner/ManageCars'
 import ManageBookings from './pages/owner/ManageBookings'
+import Login from './components/Login'
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
 
   return (
     <>
+       {ShowLogin && <Login setShowLogin={setShowLogin}/>}
        {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
 
        <Routes>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,8 +34,7 @@ const Navbar = ({setShowLogin}) => {
         
           <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
               <button onClick={()=> navigate('/owner')} className='cursor-pointer'>Dashboard</button>
-              <button onClick={()=> setShowLogin(true)} className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg
-'>Login</button>
+              <button onClick={()=> setShowLogin(true)} className='cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg'>Login</button>
           </div>
 
         </div>
